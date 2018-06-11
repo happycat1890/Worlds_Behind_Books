f16042ba1160af6e489484ff04ea9766794930bc
Molly Carlson <mollyc@email.sc.edu>
2018-06-11 13:52:05 -0400

    added content, successful transfer of content

--- Worlds_Behind_Books/ViewController.swift
+++ Worlds_Behind_Books/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fm = FileManager.default
+       let fm = FileManager.default
         let path = Bundle.main.resourcePath! //force unwrapping still required
         
         let bookTitleItems = try! fm.contentsOfDirectory(atPath: path)
@@ -25,25 +25,31 @@ class ViewController: UITableViewController {
         //distinguish item in bundle
         
         for item in bookTitleItems {
-            if item.hasPrefix("bookCover") {
+            if item.hasPrefix("book_cover") {
+                print("item confirmed")
                 bookImageTitles.append(item)
+                print(item)
                 //all the work that has to be done here
                     //rest has to be done in table-specific methods
             }
         }
-       
+ 
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookImageTitles.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //distinguish a cell object from the table view and index path
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Book Cover", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cover", for: indexPath)
         //distinguish the contents of the cell object
         cell.textLabel?.text = bookImageTitles[indexPath.row]
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    /*override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if let cvScreen = storyboard?.instantiateViewController(withIdentifier: "Cover") as? CoverViewController  {
             cvScreen.selectedCover = bookImageTitles[indexPath.row]
@@ -51,8 +57,11 @@ class ViewController: UITableViewController {
             //have to include a 'push' action from navigation controller to move to other screen
             //for now, will just include image of book cover
             navigationController?.pushViewController(cvScreen, animated: true)
+            print("pushed")
         }
     }
+    
+    */
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
