b8fc7a8e919a291b10e9d57c67305ffc21bf8ab1
Molly Carlson <mollyc@email.sc.edu>
2018-06-16 17:08:18 -0400

    tableview successfully implemented manually
    ready to push

--- Worlds_Behind_Books/CoverViewController.swift
+++ Worlds_Behind_Books/CoverViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class CoverViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
+    
     var selectedCover: String?
     @IBOutlet var imageView: UIImageView!
-    
     
     var mediaTypes = [String]()
     
@@ -26,7 +26,7 @@ class CoverViewController: UIViewController, UITableViewDelegate, UITableViewDat
             imageView.image = UIImage(named: imageToLoad)
         }
         
-       // mediaTypes += ["Art", "Music", "Social Background", "Environment", "Video Clips"]
+       
         
         
         imageView.contentMode = .scaleAspectFit
@@ -38,11 +38,33 @@ class CoverViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         self.view.addSubview(textChildView)
         
+        var tableChildView: UITableView = UITableView()
+         mediaTypes += ["Art", "Music" /*, "Environment", "Social Backgrounds","Video Clips"*/]
         
+        tableChildView = UITableView(frame: CGRect(x: 0, y: 591, width: self.view.frame.width, height: 200))
+        tableChildView.dataSource = self
+        tableChildView.delegate = self
+        //storyboard no longer required!!
+        tableChildView.register(UITableViewCell.self, forCellReuseIdentifier: "mediaType")
+        
+        //formally add
+        
+        self.view.addSubview(tableChildView)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mediaTypes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mediaType", for: indexPath)
+        //distinguish the contents of the cell object
+        cell.textLabel?.text = mediaTypes[indexPath.row]
+        return cell
+    }
         
         //mediaTypes.append("Art")
         
-       let tableChildView = UITableView(frame: CGRect(x: 0, y: 591, width: self.view.frame.width, height: 128)) // 'let' because never mutated but may change
+      /* let tableChildView = UITableView(frame: CGRect(x: 0, y: 591, width: self.view.frame.width, height: 200)) // 'let' because never mutated but may change
         tableChildView.register(UITableViewCell.self, forCellReuseIdentifier: "MediaType")
         
         tableChildView.dataSource = self
@@ -96,11 +118,12 @@ class CoverViewController: UIViewController, UITableViewDelegate, UITableViewDat
         //distinguish the contents of the cell object
         cell.textLabel?.text = mediaTypes[indexPath.row]
         return cell
-    }
+    } */
 
-    override func didReceiveMemoryWarning() {
+       override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
     }
     
     
@@ -116,4 +139,4 @@ class CoverViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     */
 
-}
+
